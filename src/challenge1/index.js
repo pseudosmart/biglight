50a8cb2c6d2f2b71d1f5d25cc5bf4eac1eef79d5
Sam Smart <sam.smart@holidayextras.com>
2021-04-06 14:34:44 +0100

    Working commit

---
+++ src/challenge1/index.js
@@ -0,0 +1,47 @@
+(() => {
+  window.onload = () => {
+    const node = document.querySelector('body')
+    const quantitySelector = document.getElementById('mobileQuantitySelection').cloneNode(true)
+    const basketButton = document.getElementById('buybox.addToCart').cloneNode(true)
+
+
+    const banner = document.createElement('div')
+    banner.className = 'banner clearfix'
+
+    const buttonContainer = document.createElement('div')
+    buttonContainer.className = 'buy-button'
+    buttonContainer.appendChild(basketButton)
+
+    banner.appendChild(quantitySelector)
+    banner.appendChild(buttonContainer)
+
+    node.appendChild(banner)
+
+    node.parentNode.insertBefore(banner, node.nextSibling)
+
+
+    const isInViewport = (boundary) => (
+      boundary.top >= 0 &&
+      boundary.left >= 0 &&
+      boundary.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+      boundary.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    )
+
+    document.addEventListener('scroll', function (e) {
+      const buyButtonBox = document.querySelector('#add-to-cart-button').getBoundingClientRect()
+      if (isInViewport(buyButtonBox)) {
+        banner.classList.add('hide')
+      } else {
+        banner.classList.remove('hide')
+      }
+    })
+
+    quantitySelector.onchange = () => {
+      const quantity = document.querySelector('.banner #mobileQuantityDropDown') //not a huge fan of this...
+      document.querySelector('#mobileQuantityDropDown').value = quantity.value
+      document.querySelector('#mobileQuantity_feature_div span.a-dropdown-prompt').innerText = quantity.value
+    }
+
+    basketButton.onclick = () => { document.getElementById('add-to-cart-button').click() } 
+  }
+})()
---
+++ src/challenge2/index.js
@@ -0,0 +1,38 @@
+((history) => {
+  window.onload = () => {
+    const banner = document.createElement('div')
+    banner.className = 'banner'
+
+    banner.innerHTML = `
+    <h3>Get 50% off <br/> all Amazon products</h3>
+    <p>Use code: <span>AZ50</span></p>
+    <p>T&Cs Apply</p>
+  `
+    insertBanner(banner)
+    history.pushState = function () {
+      waitForElementToDisplay("[data-component-type='s-search-result']", () => insertBanner(banner), 1000, 9000)
+    }
+  }
+})(window.history)
+
+const insertBanner = function (banner) {
+  const node = document.querySelector("[data-component-type='s-search-result']")
+  node.parentNode.insertBefore(banner, node.nextSibling)
+}
+
+function waitForElementToDisplay(selector, callback, checkFrequencyInMs, timeoutInMs) { //pinched from online. only required for the filters and waiting for the element to load
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    if (document.querySelector(selector) != null) {
+      callback();
+      return;
+    }
+    else {
+      setTimeout(function () {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs)
+          return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+}
